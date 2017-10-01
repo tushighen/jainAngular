ae09b2520119b00bb9a27791fdb64b09f1ecd8d8
tushighen <tushig.0803@gmail.com>
2017-10-02 02:14:51 +0800

    file upload added

--- src/app/components/ads.component.ts
+++ src/app/components/ads.component.ts
@@ -1,17 +1,31 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {AdsService} from '../services/ads.service';
 import {Router, ActivatedRoute, Route} from '@angular/router';
+import {UploadService} from "../services/upload.service";
 
 @Component({
-  selector: 'ads',
+  selector: 'app-root',
   templateUrl: '../templates/add.component.html',
-  providers: [AdsService]
+  providers: [AdsService, UploadService]
 })
 
 export class AdsComponent {
 
   constructor(private adsService: AdsService,
-              private router: Router){
+              private uploadService: UploadService,
+              private router: Router) {
+  }
+
+  @ViewChild('fileInput') fileInput;
+
+  addFile(): void {
+    let fi = this.fileInput.nativeElement;
+    if (fi.files && fi.files[0]) {
+      let fileToUpload = fi.files[0];
+      this.uploadService.uploadImage(fileToUpload).subscribe(res => {
+        console.log(res);
+      });
+    }
   }
 
   private ads: Ads;
---
+++ src/app/services/upload.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class UploadService {
+
+  constructor(private http: Http) {
+    console.log('AdsService Initialized...');
+  }
+
+  uploadImage(fileToUpload: any) {
+    let input = new FormData();
+    input.append('file', fileToUpload);
+    return this.http.post('http://localhost:8080/api/upload', input);
+  }
+}
